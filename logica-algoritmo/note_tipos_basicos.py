ff6db7f35ea2d01ab1a5cab23952a7e0e13b2d6f
one618lab <one618lab@gmail.com>
2020-03-28 22:49:57 -0300

    Conclusão aula

--- logica-algoritmo/note_tipos_basicos.py
+++ logica-algoritmo/note_tipos_basicos.py
@@ -1,4 +1,12 @@
-# Tipos Básicos - Aula 23
+# Tipos Basicos - Aula 23
 
 print(True)
 print(False)
+print(1.2 + 1) # Float + Int
+print('Texto') # String
+print("Exemplo " + 3 * "muito " + "legal") # Concatenacao
+# print(3 + "3") -> Ambiguidade
+
+print([1, 2, 3]) # Lista
+print({"nome" : "Loregian", "nasc" : 73}) # Dicionario
+print(None)
