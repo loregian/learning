cbaaa4e76a0a1dd86945969e885e328eb57c3112
one618lab <one618lab@gmail.com>
2020-03-29 16:32:04 -0300

    Nota e execícios - OPeradores

---
+++ logica-algoritmo/operadores_operacoes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    OPERADORES
+    +   Add
+    -   Sub
+    *   Mult
+    /   Div (resultado fracionário)
+    //  Div (resultado inteiro)
+    %   Módulo (resto)
+    **  Exponenciação
+"""
+# Exercício 2.1 pág 45 - Livro: "Intro à Prog com Python"
+print(10 + 20 * 30)
+print(4 ** 2 / 30)
+print((9 ** 4 + 2) * 6 -1)
+
+# Exercício 2.2 pág 45 - Livro: "Intro à Prog com Python"
+print((10 % 3) * (10 ** 2) + 1 - (10 * 4) / 2)
