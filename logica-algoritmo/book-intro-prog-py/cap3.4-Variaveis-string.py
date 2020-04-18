8f52083bfa3fc42ed8c12e45852eb0218284de2e
one618lab <one618lab@gmail.com>
2020-04-18 18:10:07 -0300

    Cap 3.4 anotações e testes

---
+++ logica-algoritmo/book-intro-prog-py/cap3.4-Variaveis-string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    Capítulo 3.4 - Variáveis String
+    Livro: "Intro à Prog com Python"
+    Autor: Nilo Ney Coutinho Menezes
+"""
+
+# len: retorna o número de caractéres na string.
+print(len("Teste"))
+print(len("Palavra"))
+
+# acessar o caracter pelo índice.
+a = "ABCD"
+print(a[0])
+print(a[3])
+print(a[2])
