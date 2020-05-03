3e6351ea9370e6dd075a341c73956f5650db34b9
loregian <alexandre.loregian@gmail.comgit config --global user.email alexandre.loregian@gmail.comqqgit config --global user.email alexandre.loregian@gmail.com>
2020-05-03 17:23:47 -0300

    Reorganizaçao de pastas JS

---
+++ JavasScript-Cod3er/fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de código")
+
+ // Início do bloco
+{
+    {
+        console.log("Olá"); // Aspas duplas - ; não obrigatório
+        console.log('Mundo') // Aspas simples
+    }
+}
+// Fim do bloco
